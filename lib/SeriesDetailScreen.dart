182a270af9c83b17e78205662617e63ae4123a89
ouzss2 <oussemamansouri4@gmail.com>
2024-10-08 09:04:13 +0100

    Movies From Api

--- lib/SeriesDetailScreen.dart
+++ lib/SeriesDetailScreen.dart
@@ -4,14 +4,14 @@ import 'Series.dart';
 class SeriesDetailScreen extends StatelessWidget {
   final Series series;
 
-  SeriesDetailScreen({required this.series});
+  const SeriesDetailScreen({super.key, required this.series});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(series.title),
-        centerTitle: true, 
+        centerTitle: true,
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -19,12 +19,15 @@ class SeriesDetailScreen extends StatelessWidget {
           Container(
             height: 300,
             width: double.infinity,
-            margin: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0), // Margins for the image
+            margin: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0), 
             child: ClipRRect(
               borderRadius: BorderRadius.circular(15.0), 
-              child: Image.asset(
-                series.imageUrl,
+              child: Image.network(
+                series.imageUrl, 
                 fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) {
+                  return const Center(child: Text('Image not found'));
+                },
               ),
             ),
           ),
@@ -33,24 +36,24 @@ class SeriesDetailScreen extends StatelessWidget {
             padding: const EdgeInsets.all(16.0),
             child: Text(
               series.title,
-              style: TextStyle(
-                fontSize: 24, // Larger font size for the title
+              style: const TextStyle(
+                fontSize: 24, 
                 fontWeight: FontWeight.bold,
               ),
-              textAlign: TextAlign.center, // Center the text
+              textAlign: TextAlign.center, 
             ),
           ),
-          // Centered description
+         
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 10.0),
             child: Text(
-              series.description,
+              series.description ?? 'No description available', 
               style: TextStyle(
                 fontSize: 16,
-                color: Colors.grey[700], // Softer color for the text
-                height: 1.5, // Adjust line spacing
+                color: Colors.grey[700], 
+                height: 1.5, 
               ),
-              textAlign: TextAlign.center, // Center the description
+              textAlign: TextAlign.center, 
               softWrap: true,
             ),
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,53 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'Series.dart';
 import 'SeriesDetailScreen.dart';
+import 'package:http/http.dart' as http ;
 
-void main() {
-  runApp(TvSeriesApp());
+void main() async {
+  List<Series> myList = await GetdataFromAPi();
+  runApp(TvSeriesApp(list:myList));
 }
 
+Future<List<Series>> GetdataFromAPi() async{
+  String url ="https://imdb-top-100-movies.p.rapidapi.com";
+
+  final header = {
+    'X-Rapidapi-Key': '75cf39f115msh5e1388559e7e8fap1b360fjsn8f1509e8c0ea',
+    'X-Rapidapi-Host': 'imdb-top-100-movies.p.rapidapi.com',
+    'Accept':'application/json'
+  };
+
+  try{
+    dynamic result = await http.get(Uri.parse(url),headers: header);
+
+    if (result.statusCode == 200){
+       List<dynamic> movies = json.decode(result.body);
+
+       return movies.map((movie)=>Series(title: movie['title'], imageUrl: movie['image'], description: movie['description'])).toList();
+    }else{
+      return [];
+    }
+  }catch(error) {
+   print(error);
+   return [];
+  }
+}
+
+
 class TvSeriesApp extends StatelessWidget {
+   List<Series> list;
+
+   TvSeriesApp({super.key, required this.list});
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       initialRoute: '/',
       routes: {
-        '/': (context) => SeriesListScreen(),
+        '/': (context) => SeriesListScreen(seriesList: list,),
       },
     );
   }
@@ -22,29 +57,14 @@ class TvSeriesApp extends StatelessWidget {
 
 class SeriesListScreen extends StatelessWidget {
 
-  final List<Series> seriesList = [
-    Series(
-      title: 'Breaking Bad',
-      imageUrl: 'images/bd.jpg',
-      description: 'Breaking Bad is a show about ...',
-    ),
-    Series(
-      title: 'Stranger Things',
-      imageUrl: 'images/st.jpg',
-      description: 'Stranger Things is a show about ...',
-    ),
-    Series(
-      title: 'Game of Thrones',
-      imageUrl: 'images/got.jpg',
-      description: 'Game of Thrones is a show about ...',
-    ),
-  ];
+   List<Series> seriesList ;
+   SeriesListScreen({super.key, required this.seriesList});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Popular TV Series'),
+        title: const Text('Popular TV Series'),
       ),
       body: ListView.builder(
         itemCount: seriesList.length,
@@ -76,7 +96,7 @@ class SeriesCard extends StatelessWidget {
   final String title;
   final String imageUrl;
 
-  SeriesCard({required this.title, required this.imageUrl});
+  const SeriesCard({super.key, required this.title, required this.imageUrl});
 
   @override
   Widget build(BuildContext context) {
@@ -84,19 +104,19 @@ class SeriesCard extends StatelessWidget {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(15), // Coins arrondis pour la carte
       ),
-      margin: EdgeInsets.all(10),
+      margin: const EdgeInsets.all(10),
       elevation: 5,
       child: Column(
         children: [
           // Section image avec coins arrondis
           ClipRRect(
-            borderRadius: BorderRadius.vertical(
+            borderRadius: const BorderRadius.vertical(
               top: Radius.circular(15), // Coins supérieurs arrondis
             ),
-            child: Container(
+            child: SizedBox(
               height: 200,
               width: double.infinity,
-              child: Image.asset(
+              child: Image.network(
                 imageUrl,
                 fit: BoxFit.cover, // Assurez-vous que l'image couvre bien l'espace
               ),
@@ -107,7 +127,7 @@ class SeriesCard extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Text(
               title,
-              style: TextStyle(
+              style: const TextStyle(
                 fontSize: 18,
                 fontWeight: FontWeight.bold,
               ),
